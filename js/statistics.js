71516b58649cc9298d8ec0cc40438e552c10023f
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-07-19 15:24:44 +0300

    add a row

--- js/statistics.js
+++ js/statistics.js
@@ -1,57 +1,58 @@
 
 'use strict';
 
+/*
+function showData() {
 
-const menu = [];
+            const myTable = document.getElementById('myTable');
+            const newRow = myTable.insertRow();
+            const cell1 = newRow.insertCell(0);
+            const cell2 = newRow.insertCell(1);
+            const cell3 = newRow.insertCell(2);
+            const cell4 = newRow.insertCell(3);
 
-function Food(foodId, foodName,foodType, price) {
+            cell1.innerHTML = parsedData[i].foodId;
+            cell2.textContent = parsedData[i].foodName;
+            cell3.textContent = parsedData[i].foodType;
+            cell4.textContent = parsedData[i].price;
+        
+}*/
 
-    this.foodId = foodId;
-    this.foodName = foodName;
-    this.foodType = foodType;
-    this.price = price;
 
-    menu.push(this);
-    getData();
-}
+function getData() {
 
- Food.prototype.render = function (){
-   
-    
-    const myTable = document.getElementById('myTable');
-    const newRow = myTable.insertRow(1);
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-    const cell4 = newRow.insertCell(3);
+    let retrivedData = localStorage.getItem("menu");
+    let parsedData = JSON.parse(retrivedData);
+    console.log(parsedData);
 
-    cell1.textContent = foodId;
-    cell2.textContent = foodName;
-    cell3.textContent = foodType;
-    cell4.textContent = price;
+    if (parsedData != null) {
+
+        
+
+        for (let i = 0; i < parsedData.length; i++) {
+            
+            const newRow = myTable.insertRow();
+            const cell1 = newRow.insertCell(0);
+            const cell2 = newRow.insertCell(1);
+            const cell3 = newRow.insertCell(2);
+            const cell4 = newRow.insertCell(3);
+
+            cell1.innerHTML = parsedData[i].foodId;
+            cell2.textContent = parsedData[i].foodName;
+            cell3.textContent = parsedData[i].foodType;
+            cell4.textContent = parsedData[i].price;
+            
+        }
+
     }
 
-    
-    function getData() {
-    
-        let retrivedData =localStorage.getItem("menu");
-        let parsedData = JSON.parse(retrivedData);
-        console.log(parsedData);
-        if(retrivedData){
-            for(let i=0 ; i< retrivedData.length; i++){
-               retrivedData[i].render();
-            }
-        }
-    
-    
-    } 
-    
-   getData();
-  
+}
 
-   
-  
+const myTable = document.getElementById('myTable');
+getData();
 
 
 
- 
+
+
+
