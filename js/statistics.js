4a57f4cf52f67197508501cd4f118bcf8b48f833
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-07-19 22:32:40 +0300

    add new line

--- js/statistics.js
+++ js/statistics.js
@@ -1,36 +1,13 @@
 
-'use strict';
-
-/*
-function showData() {
-
-            const myTable = document.getElementById('myTable');
-            const newRow = myTable.insertRow();
-            const cell1 = newRow.insertCell(0);
-            const cell2 = newRow.insertCell(1);
-            const cell3 = newRow.insertCell(2);
-            const cell4 = newRow.insertCell(3);
-
-            cell1.innerHTML = parsedData[i].foodId;
-            cell2.textContent = parsedData[i].foodName;
-            cell3.textContent = parsedData[i].foodType;
-            cell4.textContent = parsedData[i].price;
-        
-}*/
-
+const myTable = document.getElementById('myTable');
 
 function getData() {
-
     let retrivedData = localStorage.getItem("menu");
     let parsedData = JSON.parse(retrivedData);
     console.log(parsedData);
 
     if (parsedData != null) {
-
-        
-
         for (let i = 0; i < parsedData.length; i++) {
-            
             const newRow = myTable.insertRow();
             const cell1 = newRow.insertCell(0);
             const cell2 = newRow.insertCell(1);
@@ -41,18 +18,8 @@ function getData() {
             cell2.textContent = parsedData[i].foodName;
             cell3.textContent = parsedData[i].foodType;
             cell4.textContent = parsedData[i].price;
-            
         }
-
     }
-
 }
 
-const myTable = document.getElementById('myTable');
 getData();
-
-
-
-
-
-
